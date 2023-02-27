1a032a86be033ac411a915864113b815bed88225
AltynZhanbyr <altyn.bekzat01@gmail.com>
2023-02-27 22:54:54 +0600

    FEAT Shopping List 4#: Bottom navigation view added

--- app/src/main/java/com/example/shoppinglist/activities/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/activities/MainActivity.kt
@@ -3,11 +3,45 @@ package com.example.shoppinglist.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.shoppinglist.R
+import com.example.shoppinglist.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding:ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
+        setBottomNavigationView()
+    }
+
+    private fun setBottomNavigationView(){
+        binding?.bottomNavigationMenu?.setOnItemSelectedListener {
+            var mes = ""
+            when(it.itemId){
+                R.id.settings -> {
+                    mes = "Settings"
+                }
+                R.id.notes-> {
+                    mes = "Notes"
+                }
+                R.id.shop_list->{
+                    mes = "Shop List"
+                }
+                R.id.new_item->{
+                    mes = "New"
+                }
+            }
+            Snackbar.make(binding?.root?.rootView!!, "You clicked on $mes button",Snackbar.LENGTH_SHORT).show()
+            true
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
     }
 }
